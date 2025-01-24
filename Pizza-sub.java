48faff324c08ea6c456c29b971da344594114c27
pedrokekeww <pedroveb119@gmail.com>
2025-01-24 14:42:30 -0300

    Create Pizza-sub.java

---
+++ Pizza-sub.java
@@ -0,0 +1,49 @@
+package pizzaria;
+
+public class Pizza extends Alimentos
+{
+	private String borda;
+	private double fatia;
+	
+	public Pizza(int estoque) {
+		super(estoque);
+		System.out.println("Alimento: Pizza!");
+	}
+	public Pizza() {
+		super();
+		System.out.println("Alimento: Pizza!");
+	}
+	public String getBorda() {
+		return borda;
+	}
+
+	public void setBorda(String borda) {
+		this.borda = borda;
+	}
+
+	public double getFatia() {
+		return fatia;
+	}
+
+	public void setFatia(double fatia) {
+		this.fatia = fatia;
+	}
+	@Override
+	public String toString() 
+	{
+		return "Pizza" + "\nborda = " + borda + "\nfatia = " + fatia;
+	}
+
+	public String numFatiaspPessoa(double pessoas) 
+	{
+		if (fatia >= pessoas) 
+		{
+			double result = fatia/pessoas;
+			return "A quantidade de fatias para cada pessoa é " +result+ "!!";
+		}
+		else 
+		{
+			return "Compre mais pizza.";
+		}
+	}
+}
